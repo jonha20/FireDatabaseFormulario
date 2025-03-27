14ababcb6bddd2c13047ce590b13eec2a7b81e6f
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-03-27 20:09:39 +0100

    Ejercicio solucionado

---
+++ js/script.js
@@ -0,0 +1,121 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBjTSABchzKfRpzqMCvUiZT-c-0GTCAtnc",
+  authDomain: "pruebadatabase-7e515.firebaseapp.com",
+  projectId: "pruebadatabase-7e515",
+  storageBucket: "pruebadatabase-7e515.firebasestorage.app",
+  messagingSenderId: "299548159984",
+  appId: "1:299548159984:web:6493e90ad4f315439ff735",
+  measurementId: "G-TBR64S993K",
+};
+
+firebase.initializeApp(firebaseConfig); // Inicializaar app Firebase
+
+const db = firebase.firestore(); // db representa mi BBDD //inicia Firestore
+// Array para almacenar los contactos//
+let contactos = [];
+db.collection("usuarios")
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, doc.data().mensaje);
+    });
+  });
+const printData = () => {
+  //Petición a Firestore para leer todos los documentos de la colección album
+  const listaContactos = document.getElementById("listaContactos");
+  listaContactos.innerHTML = "";
+
+  db.collection("usuarios")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc, id) => {
+        const contactoDiv = document.createElement("div");
+        
+        contactoDiv.innerHTML = `
+            <p>Nombre: ${doc.data().nombre}</p>
+            <p>Email: ${doc.data().email}</p>
+            <p>Mensaje: ${doc.data().mensaje}</p>
+            <p>URL: ${doc.data().url}</p>
+            <p>ID: ${doc.id}</p>
+            <button id="delete" type="button">Borrar</button>
+            <hr>
+        `;
+        listaContactos.appendChild(contactoDiv);
+      });
+    });
+};
+const readAll = (array) => {
+  db.collection("usuarios")
+    .add(array)
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+      printData();
+    })
+    .catch((error) => console.error("Error adding document: ", error));
+};
+
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Obtener valores de el formulario//
+  const nuevoContacto = {
+    nombre: document.getElementById("nombre").value,
+    email: document.getElementById("email").value,
+    mensaje: document.getElementById("mensaje").value,
+    url: document.getElementById("url").value,
+  };
+  readAll(nuevoContacto);
+});
+// Borrar todos los contactos
+document.getElementById("borrarTodo").addEventListener("click", function () {
+    db.collection("usuarios")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        doc.ref.delete();
+      });
+    });
+  //printData();
+  //window.location.reload();
+  contactos = [];
+  //readAll();
+  alert("Todos los contactos han sido eliminados");
+});/*
+document.getElementById("delete").addEventListener("click", function() {
+    alert("nobxis")
+    db.collection("usuarios")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        doc.ref.delete();
+      });
+    });
+  });
+*/
+  document.getElementById("borrarSeleccionado").addEventListener("click", function () {
+  alert("iefbibo")
+    const id = prompt('Introduce el ID a borrar');
+  db.collection('usuarios').doc(id).delete().then(() => {
+    alert(`Usuario ${id} ha sido borrado`);
+    //Clean
+    document.getElementById('listaContactos').innerHTML = "";
+    //Read all again¡
+  })
+    .catch(() => console.log('Error borrando documento'));
+
+  })
+/*function borrarContacto() {
+    db.collection("usuarios")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        doc.ref.delete();
+      });
+    });
+  
+ // printData();
+
+  alert("Todos los contactos han sido eliminados");
+}
+
+*/
